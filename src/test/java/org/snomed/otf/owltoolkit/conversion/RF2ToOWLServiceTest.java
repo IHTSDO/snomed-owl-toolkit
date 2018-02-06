b6484539ee4ee1030c97560c4fba3a5ac2414c5d
Kai Kewley <kaikewley@gmail.com>
2018-02-06 12:19:43 +0000

    CLASS-79 Add RF2 to OWL integration test.

---
+++ src/test/java/org/snomed/otf/owltoolkit/conversion/RF2ToOWLServiceTest.java
@@ -0,0 +1,96 @@
+package org.snomed.otf.owltoolkit.conversion;
+
+import org.junit.Test;
+import org.snomed.otf.owltoolkit.testutil.ZipUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import static org.junit.Assert.*;
+
+public class RF2ToOWLServiceTest {
+
+	@Test
+	public void convertRF2ArchiveToOWL() throws Exception {
+		final RF2ToOWLService rf2ToOWLService = new RF2ToOWLService();
+
+		File baseRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Base_snapshot");
+
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		rf2ToOWLService.convertRF2ArchiveToOWL(new FileInputStream(baseRF2SnapshotZip), byteArrayOutputStream);
+
+		assertEquals(
+						"Prefix(:=<http://snomed.info/id/>)\n" +
+						"Prefix(owl:=<http://www.w3.org/2002/07/owl#>)\n" +
+						"Prefix(rdf:=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>)\n" +
+						"Prefix(xml:=<http://www.w3.org/XML/1998/namespace>)\n" +
+						"Prefix(xsd:=<http://www.w3.org/2001/XMLSchema#>)\n" +
+						"Prefix(rdfs:=<http://www.w3.org/2000/01/rdf-schema#>)\n" +
+						"\n" +
+						"\n" +
+						"Ontology(<http://snomed.info/id/>\n" +
+						"\n" +
+						"Declaration(Class(<http://snomed.info/id/113331007>))\n" +
+						"Declaration(Class(<http://snomed.info/id/116680003>))\n" +
+						"Declaration(Class(<http://snomed.info/id/138875005>))\n" +
+						"Declaration(Class(<http://snomed.info/id/362969004>))\n" +
+						"Declaration(Class(<http://snomed.info/id/363698007>))\n" +
+						"Declaration(Class(<http://snomed.info/id/404684003>))\n" +
+						"Declaration(Class(<http://snomed.info/id/410662002>))\n" +
+						"Declaration(Class(<http://snomed.info/id/723594008>))\n" +
+						"Declaration(Class(<http://snomed.info/id/723596005>))\n" +
+						"Declaration(Class(<http://snomed.info/id/900000000000441003>))\n" +
+						"Declaration(ObjectProperty(<http://snomed.info/id/363698007>))\n" +
+						"Declaration(ObjectProperty(<http://snomed.info/id/609096000>))\n" +
+						"\n" +
+						"############################\n" +
+						"#   Classes\n" +
+						"############################\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/113331007> (<http://snomed.info/id/113331007>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/113331007> <http://snomed.info/id/138875005>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/116680003> (<http://snomed.info/id/116680003>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/116680003> <http://snomed.info/id/900000000000441003>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/138875005> (<http://snomed.info/id/138875005>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/138875005> owl:Thing)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/362969004> (<http://snomed.info/id/362969004>)\n" +
+						"\n" +
+						"EquivalentClasses(<http://snomed.info/id/362969004> ObjectIntersectionOf(<http://snomed.info/id/404684003> ObjectSomeValuesFrom(<http://snomed.info/id/609096000> ObjectSomeValuesFrom(<http://snomed.info/id/363698007> <http://snomed.info/id/113331007>))))\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/363698007> (<http://snomed.info/id/363698007>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/363698007> <http://snomed.info/id/410662002>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/404684003> (<http://snomed.info/id/404684003>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/404684003> <http://snomed.info/id/138875005>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/410662002> (<http://snomed.info/id/410662002>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/410662002> <http://snomed.info/id/900000000000441003>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/723594008> (<http://snomed.info/id/723594008>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/723594008> <http://snomed.info/id/900000000000441003>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/723596005> (<http://snomed.info/id/723596005>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/723596005> <http://snomed.info/id/723594008>)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/900000000000441003> (<http://snomed.info/id/900000000000441003>)\n" +
+						"\n" +
+						"SubClassOf(<http://snomed.info/id/900000000000441003> <http://snomed.info/id/138875005>)\n" +
+						"\n" +
+						"\n" +
+						")",
+				byteArrayOutputStream.toString());
+	}
+
+}
