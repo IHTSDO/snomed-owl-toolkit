9ee5d07587ba2d00ca6e5091265923379a3afc99
Kai Kewley <kaikewley@gmail.com>
2018-07-06 15:23:08 +0100

    CLASS-86 Snomed Extension RF2 to OWL test with custom ontology header.

--- src/main/java/org/snomed/otf/owltoolkit/util/InputStreamSet.java
+++ src/main/java/org/snomed/otf/owltoolkit/util/InputStreamSet.java
@@ -3,12 +3,18 @@ package org.snomed.otf.owltoolkit.util;
 import com.google.common.collect.Sets;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class InputStreamSet extends HashSet<InputStream> implements AutoCloseable {
 
 	private final Set<InputStream> fileInputStreams;
+
+	public InputStreamSet(java.io.File... files) throws FileNotFoundException {
+		this(new HashSet<>(Arrays.asList(files)));
+	}
 
 	public InputStreamSet(Set<java.io.File> files) throws FileNotFoundException {
 		fileInputStreams = new HashSet<>();
--- src/test/java/org/snomed/otf/owltoolkit/conversion/RF2ToOWLServiceTest.java
+++ src/test/java/org/snomed/otf/owltoolkit/conversion/RF2ToOWLServiceTest.java
@@ -1,5 +1,6 @@
 package org.snomed.otf.owltoolkit.conversion;
 
+import com.google.common.collect.Sets;
 import org.junit.Test;
 import org.snomed.otf.owltoolkit.util.InputStreamSet;
 import org.snomed.otf.owltoolkit.util.OptionalFileInputStream;
@@ -116,5 +117,125 @@ public class RF2ToOWLServiceTest {
 						")",
 				byteArrayOutputStream.toString());
 	}
+
+	@Test
+	public void convertExtensionRF2ArchivesToOWL() throws Exception {
+		final RF2ToOWLService rf2ToOWLService = new RF2ToOWLService();
+
+		File baseRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Base_snapshot");
+		File extensionRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Extension_snapshot");
+
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		rf2ToOWLService.convertRF2ArchiveToOWL(null, "20180731", true, new InputStreamSet(baseRF2SnapshotZip, extensionRF2SnapshotZip), new OptionalFileInputStream(null), byteArrayOutputStream);
+
+		assertEquals("" +
+						"Prefix(test:=<http://test.com/test/>)\n" +
+						"Prefix(:=<http://snomed.info/id/>)\n" +
+						"Prefix(owl:=<http://www.w3.org/2002/07/owl#>)\n" +
+						"Prefix(rdf:=<http://www.w3.org/1999/02/22-rdf-syntax-ns#>)\n" +
+						"Prefix(xml:=<http://www.w3.org/XML/1998/namespace>)\n" +
+						"Prefix(xsd:=<http://www.w3.org/2001/XMLSchema#>)\n" +
+						"Prefix(rdfs:=<http://www.w3.org/2000/01/rdf-schema#>)\n" +
+						"\n" +
+						"\n" +
+
+				// Extension Ontology header
+
+						"Ontology(<http://snomed.info/sct/900101001>\n" +
+						"<http://snomed.info/sct/900101001/version/20180731>\n" +
+
+						"\n" +
+						"Declaration(Class(:113331007))\n" +
+						"Declaration(Class(:116680003))\n" +
+						"Declaration(Class(:138875005))\n" +
+						"Declaration(Class(:362969004))\n" +
+						"Declaration(Class(:363698007))\n" +
+						"Declaration(Class(:404684003))\n" +
+						"Declaration(Class(:410662002))\n" +
+						"Declaration(Class(:723594008))\n" +
+						"Declaration(Class(:723596005))\n" +
+						"Declaration(Class(:900000000000441003))\n" +
+
+				// Additional Extension class declaration
+						"Declaration(Class(:900101001))\n" +
+
+						"Declaration(ObjectProperty(:363698007))\n" +
+						"Declaration(ObjectProperty(:609096000))\n" +
+						"\n" +
+						"############################\n" +
+						"#   Object Properties\n" +
+						"############################\n" +
+						"\n" +
+						"# Object Property: <http://snomed.info/id/363698007> (Finding site (attribute))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :363698007 \"Finding site (attribute)\"^^xsd:string)\n" +
+						"\n" +
+						"\n" +
+						"\n" +
+						"############################\n" +
+						"#   Classes\n" +
+						"############################\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/113331007> (Structure of endocrine system (body structure))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :113331007 \"Structure of endocrine system (body structure)\"^^xsd:string)\n" +
+						"SubClassOf(:113331007 :138875005)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/116680003> (Is a (attribute))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :116680003 \"Is a (attribute)\"^^xsd:string)\n" +
+						"SubClassOf(:116680003 :900000000000441003)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/138875005> (SNOMED CT Concept (SNOMED RT+CTV3))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :138875005 \"SNOMED CT Concept (SNOMED RT+CTV3)\"^^xsd:string)\n" +
+						"SubClassOf(:138875005 owl:Thing)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/362969004> (Disorder of endocrine system (disorder))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :362969004 \"Disorder of endocrine system (disorder)\"^^xsd:string)\n" +
+						"EquivalentClasses(:362969004 ObjectIntersectionOf(:404684003 ObjectSomeValuesFrom(:609096000 ObjectSomeValuesFrom(:363698007 :113331007))))\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/363698007> (Finding site (attribute))\n" +
+						"\n" +
+						"SubClassOf(:363698007 :410662002)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/404684003> (Clinical finding (finding))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :404684003 \"Clinical finding (finding)\"^^xsd:string)\n" +
+						"SubClassOf(:404684003 :138875005)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/410662002> (Concept model attribute (attribute))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :410662002 \"Concept model attribute (attribute)\"^^xsd:string)\n" +
+						"SubClassOf(:410662002 :900000000000441003)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/723594008> (All precoordinated SNOMED CT content (foundation metadata concept))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :723594008 \"All precoordinated SNOMED CT content (foundation metadata concept)\"^^xsd:string)\n" +
+						"SubClassOf(:723594008 :900000000000441003)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/723596005> (All SNOMED CT content (foundation metadata concept))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :723596005 \"All SNOMED CT content (foundation metadata concept)\"^^xsd:string)\n" +
+						"SubClassOf(:723596005 :723594008)\n" +
+						"\n" +
+						"# Class: <http://snomed.info/id/900000000000441003> (SNOMED CT Model Component (metadata))\n" +
+						"\n" +
+						"AnnotationAssertion(rdfs:label :900000000000441003 \"SNOMED CT Model Component (metadata)\"^^xsd:string)\n" +
+						"SubClassOf(:900000000000441003 :138875005)\n" +
+						"\n" +
+
+				// Additional Extension class
+
+						"# Class: <http://snomed.info/id/900101001> (<http://snomed.info/id/900101001>)\n" +
+						"\n" +
+						"SubClassOf(:900101001 :900000000000441003)\n" +
+						"\n" +
+						"\n" +
+						")",
+				byteArrayOutputStream.toString());
+	}
+
 
 }
