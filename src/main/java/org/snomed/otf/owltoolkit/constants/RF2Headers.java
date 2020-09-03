140bb267401675665121132eeb4bc728ae9d3f71
Kai Kewley <kaikewley@gmail.com>
2020-09-03 20:03:02 +0100

    MAINT-1444 Write descriptions, lang refset and axioms to RF2 archive.
    Co-Authored-By: Warren Del-Pinto <29575123+W-D-P@users.noreply.github.com>

--- src/main/java/org/snomed/otf/owltoolkit/constants/RF2Headers.java
+++ src/main/java/org/snomed/otf/owltoolkit/constants/RF2Headers.java
@@ -4,6 +4,7 @@ public class RF2Headers {
 	public static final String CONCEPT_HEADER = "id\teffectiveTime\tactive\tmoduleId\tdefinitionStatusId";
 	public static final String DESCRIPTION_HEADER = "id\teffectiveTime\tactive\tmoduleId\tconceptId\tlanguageCode\ttypeId\tterm\tcaseSignificanceId";
 	public static final String RELATIONSHIP_HEADER = "id\teffectiveTime\tactive\tmoduleId\tsourceId\tdestinationId\trelationshipGroup\ttypeId\tcharacteristicTypeId\tmodifierId";
+	public static final String LANGUAGE_REFERENCE_SET_HEADER = "id\teffectiveTime\tactive\tmoduleId\trefsetId\treferencedComponentId\tacceptabilityId";
 	public static final String OWL_EXPRESSION_REFERENCE_SET_HEADER = "id\teffectiveTime\tactive\tmoduleId\trefsetId\treferencedComponentId\towlExpression";
 
 }
