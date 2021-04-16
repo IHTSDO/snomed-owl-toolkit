52ce5a0b0506a6bb53a2bc067ed2cb57676aee06
Kai Kewley <kaikewley@gmail.com>
2021-04-16 15:29:31 +0100

    ISTO-62 Simple incremental NNF attribute subsumtion test.

--- src/test/java/org/snomed/otf/owltoolkit/service/classification/AxiomModificationClassificationIntegrationTest.java
+++ src/test/java/org/snomed/otf/owltoolkit/service/classification/AxiomModificationClassificationIntegrationTest.java
@@ -33,7 +33,7 @@ import static org.snomed.otf.owltoolkit.service.classification.TestFileUtil.read
 
 public class AxiomModificationClassificationIntegrationTest {
 
-	private SnomedReasonerService snomedReasonerService = new SnomedReasonerService();
+	private final SnomedReasonerService snomedReasonerService = new SnomedReasonerService();
 
 	@Test
 	public void testClassifyStatedRelationshipAndAxiomSnapshotWithoutChanges() throws IOException, ReasonerServiceException {
@@ -77,48 +77,6 @@ public class AxiomModificationClassificationIntegrationTest {
 
 		// Assert results
 		List<String> lines = readInferredRelationshipLinesTrim(results);
-		assertEquals(2, lines.size());
-
-		// Existing attribute is made inactive - Disorder of endocrine system - Finding site - Structure of endocrine system
-		assertTrue(lines.contains("200111001\t\t0\t\t362969004\t113331007\t1\t363698007\t900000000000011006\t900000000000451002"));
-	}
-
-	@Test
-	public void testIncrementallyClassifyAxiomSnapshotWithoutDeltaChangeLeadingToRedundantAttribute() throws IOException, ReasonerServiceException {
-		File baseRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Base_CompleteOwl_snapshot");
-		assertNotNull(snomedReasonerService);
-
-
-		// Run initial classification
-		File results = TestFileUtil.newTemporaryFile();
-		final ClassificationContainer classificationContainer = snomedReasonerService.classify("", baseRF2SnapshotZip, null, results, ELK_REASONER_FACTORY, false);
-
-		// Assert results
-		List<String> lines = readInferredRelationshipLinesTrim(results);
-		assertEquals(1, lines.size());
-
-
-		// Run incremental classification using existing container and reasoner ontology
-		File deltaZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Change_Axiom_Parents_delta");
-		results = TestFileUtil.newTemporaryFile();
-		snomedReasonerService.updateClassification(classificationContainer, new FileInputStream(deltaZip), new FileOutputStream(results));
-
-		// Assert results
-		lines = readInferredRelationshipLinesTrim(results);
-		assertEquals(2, lines.size());
-
-		// Existing attribute is made inactive - Disorder of endocrine system - Finding site - Structure of endocrine system
-		assertTrue(lines.contains("200111001\t\t0\t\t362969004\t113331007\t1\t363698007\t900000000000011006\t900000000000451002"));
-
-
-		// Run incremental classification again
-		// Because we have not saved the inferred relationships from the last classification we get the same results again
-		deltaZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Change_Axiom_Parents_delta");
-		results = TestFileUtil.newTemporaryFile();
-		snomedReasonerService.updateClassification(classificationContainer, new FileInputStream(deltaZip), new FileOutputStream(results));
-
-		// Assert results
-		lines = readInferredRelationshipLinesTrim(results);
 		assertEquals(2, lines.size());
 
 		// Existing attribute is made inactive - Disorder of endocrine system - Finding site - Structure of endocrine system
