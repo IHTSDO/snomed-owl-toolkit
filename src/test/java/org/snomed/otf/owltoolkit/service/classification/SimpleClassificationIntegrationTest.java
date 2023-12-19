145354100f9660591d183e2126fbe61508573a32
Quyen Ly <quocquyenit@gmail.com>
2023-12-19 13:28:30 +0700

    MAINT-2416 Allow parsing of SubAnnotationPropertyOf when classifying

--- src/test/java/org/snomed/otf/owltoolkit/service/classification/SimpleClassificationIntegrationTest.java
+++ src/test/java/org/snomed/otf/owltoolkit/service/classification/SimpleClassificationIntegrationTest.java
@@ -78,6 +78,23 @@ public class SimpleClassificationIntegrationTest {
 		assertTrue(lines.contains("1\t\t73211009\t113331007\t0\t" + FINDING_SITE + "\t900000000000011006\t900000000000451002"));
 	}
 
+	@Test
+	public void testClassifyAnnotationAttributes() throws IOException, ReasonerServiceException {
+		File baseRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Base_snapshot");
+		File deltaZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Add_Annotation_Property_delta");
+		assertNotNull(snomedReasonerService);
+
+		// Run classification
+		File results = TestFileUtil.newTemporaryFile();
+		snomedReasonerService.classify("", baseRF2SnapshotZip, deltaZip, results, ELK_REASONER_FACTORY, false);
+
+		// Assert results
+		List<String> lines = readInferredRelationshipLinesTrim(results);
+		assertEquals(3, lines.size());
+		assertTrue(lines.contains("1\t\t3113249002\t1295447006\t0\t" + Concepts.IS_A+ "\t900000000000011006\t900000000000451002"));
+		assertTrue(lines.contains("1\t\t1295449009\t1295447006\t0\t" + Concepts.IS_A+ "\t900000000000011006\t900000000000451002"));
+	}
+
 
 	@Test
 	public void testClassifyConceptInactivation() throws IOException, ReasonerServiceException {
